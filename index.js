28a1483a0480ff26e5f5ea8480f9e99acdede9a0
NSpiros <126255001+NSpiros@users.noreply.github.com>
2023-03-22 07:15:04 +0200

    Added button functions to get choices and display them

--- index.js
+++ index.js
@@ -1,88 +1,81 @@
 //Rock-Paper-Scissors
 
-const Rock = "rock";
-const Paper = "paper";
-const Scissors = "scissors";
-
+const playerSelection = document.querySelectorAll("button");
+const playerChoiceDisplay = document.querySelector(".player-selection");
+const computerChoiceDisplay = document.querySelector(".computer-selection");
 let playerWins = 0;
 let computerWins = 0;
-
-let playerSelection;
-let computerSelection;
 
 function getComputerChoice() {
   randomChoice = Math.floor(Math.random() * 3);
 
   switch (randomChoice) {
     case 0:
-      return Rock;
-      break;
+      return "Rock";
     case 1:
-      return Paper;
+      return "Paper";
     case 2:
-      return Scissors;
-
+      return "Scissors";
     default:
       break;
   }
 }
 
-//get input from user
-//validate selection and spellchecking
-
-function getPlayerChoice() {
-  let choiceInput = window.prompt("Pick : (Rock,Paper,Scissors)");
-  if (choiceInput !== null && choiceInput !== undefined) {
-    if (
-      choiceInput.toLowerCase() === "rock" ||
-      choiceInput.toLowerCase() === "paper" ||
-      choiceInput.toLowerCase() === "scissors"
-    ) {
-      return choiceInput.toLowerCase();
-    } else {
-      return;
-    }
-    console.log("Please pick rock paper or scissors");
-  }
+//Get player-computer choices!
+//Display choices
+//return choices
+function getChoices(e) {
+  let playerSelection = e.target.parentNode.id;
+  let computerSelection = getComputerChoice();
+  playerChoiceDisplay.firstElementChild.setAttribute(
+    "src",
+    `./assets/${playerSelection}-player.png`
+  );
+  computerChoiceDisplay.firstElementChild.setAttribute(
+    "src",
+    `./assets/${computerSelection}-com.png`
+  );
 }
 
-function playRound(playerSelection, computerSelection) {
-  playerSelection = getPlayerChoice();
-  computerSelection = getComputerChoice();
-  if (playerSelection !== null && playerSelection !== undefined) {
-    if (playerSelection !== computerSelection) {
-      if (
-        (playerSelection === "rock" && computerSelection === "scissors") ||
-        (playerSelection === "paper" && computerSelection === "rock") ||
-        (playerSelection === "scissors" && computerSelection === "paper")
-      ) {
-        console.log(`You win!${playerSelection} wins ${computerSelection}`);
-        playerWins++;
-      } else {
-        console.log(`You lost!${computerSelection} wins ${playerSelection}`);
-        computerWins++;
-      }
-    } else {
-      console.log(`It's a tie!${playerSelection} ties to ${computerSelection}`);
-      return;
-    }
-  } else {
-    console.log(`Please type : rock, paper, scissors`);
-    return;
-  }
-}
-// playRound(playerSelection, computerSelection);
+playerSelection.forEach((button) => {
+  button.addEventListener("click", getChoices);
+});
 
-function game() {
-  for (let i = 0; i < 5; i++) {
-    let result = playRound(playerSelection, computerSelection);
-    console.log(`turn ${i}`);
-    console.log(playerWins, computerWins);
-  }
-  if (playerWins > computerWins && playerWins != computerWins) {
-    console.log(`You win ${playerWins} : ${computerWins}`);
-  } else {
-    console.log(`You Lose ${computerWins} : ${playerWins}`);
-  }
-}
-game();
+// function playRound(getPlayerChoice, getComputerChoice) {
+//   if (playerSelection !== null && playerSelection !== undefined) {
+//     if (playerSelection !== computerSelection) {
+//       if (
+//         (playerSelection === "rock" && computerSelection === "scissors") ||
+//         (playerSelection === "paper" && computerSelection === "rock") ||
+//         (playerSelection === "scissors" && computerSelection === "paper")
+//       ) {
+//         console.log(`You win!${playerSelection} wins ${computerSelection}`);
+//         playerWins++;
+//       } else {
+//         console.log(`You lost!${computerSelection} wins ${playerSelection}`);
+//         computerWins++;
+//       }
+//     } else {
+//       console.log(`It's a tie!${playerSelection} ties to ${computerSelection}`);
+//       return;
+//     }
+//   } else {
+//     console.log(`Please type : rock, paper, scissors`);
+//     return;
+//   }
+// }
+// // playRound(playerSelection, computerSelection);
+
+// function game() {
+//   for (let i = 0; i < 5; i++) {
+//     let result = playRound(playerSelection, computerSelection);
+//     console.log(`turn ${i}`);
+//     console.log(playerWins, computerWins);
+//   }
+//   if (playerWins > computerWins && playerWins != computerWins) {
+//     console.log(`You win ${playerWins} : ${computerWins}`);
+//   } else {
+//     console.log(`You Lose ${computerWins} : ${playerWins}`);
+//   }
+// }
+// // game();
