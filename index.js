61b48d191b5a5d6a158d0c37714ff6bc549b608e
NSpiros <126255001+NSpiros@users.noreply.github.com>
2023-03-31 16:06:26 +0300

    first keyboard version

--- index.js
+++ index.js
@@ -1,6 +1,24 @@
-window.addEventListener("keydown", function (e) {
-  console.log(e.key);
-  if (e.key === "Enter") {
-    window.location.href = "./GamePage/game-page.html";
-  }
+const keyboardButtons = document.querySelectorAll(".btn");
+const textarea = document.querySelector("textarea");
+const deleteButton = document.querySelector(".delete");
+const capsButton = document.querySelector(".caps");
+
+let chars = [];
+
+keyboardButtons.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    textarea.value += btn.innerText; //display buttons in textarea
+    chars = textarea.value.split("");
+  });
+});
+
+deleteButton.addEventListener("click", () => {
+  chars.pop();
+  textarea.value = chars.join("");
+});
+
+capsButton.addEventListener("click", () => {
+  keyboardButtons.forEach((btn) => {
+    btn.classList.toggle("upper");
+  });
 });
