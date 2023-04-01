31811c3758a12d0fa75c6da282939a71a85f7027
NSpiros <126255001+NSpiros@users.noreply.github.com>
2023-04-01 17:40:38 +0300

    Finished off username section

--- index.js
+++ index.js
@@ -2,7 +2,11 @@ const keyboardButtons = document.querySelectorAll(".btn");
 const textarea = document.querySelector("textarea");
 const deleteButton = document.querySelector(".delete");
 const capsButton = document.querySelector(".caps");
+const submitButton = document.querySelector(".submit__btn");
+const userNameScreen = document.querySelector(".username__screen");
+const introScreen = document.querySelector(".intro");
 
+let userName;
 let chars = [];
 
 keyboardButtons.forEach((btn) => {
@@ -21,4 +25,9 @@ capsButton.addEventListener("click", () => {
   keyboardButtons.forEach((btn) => {
     btn.classList.toggle("upper");
   });
+});
+submitButton.addEventListener("click", () => {
+  userName = chars.join("");
+  userNameScreen.classList.replace("fadeIn", "fadeOut");
+  console.log(userName);
 });
