b16115084bfeb7fc59ec0d9ab2430a355553be8f
NSpiros <126255001+NSpiros@users.noreply.github.com>
2023-04-09 18:54:09 +0300

    added ui fixes and completed versus screen animation

--- index.js
+++ index.js
@@ -76,6 +76,9 @@ const startGameButton = document.querySelector('.start__game__container');
 const vsLeftSide = document.querySelector('.left-half');
 const vsRightSide = document.querySelector('.right-half');
 const vsMiddleText = document.querySelector('.versus__text');
+const vsPlayerAvatar = document.querySelector('.versus-img-player');
+const vsComAvatar = document.querySelector('.versus-img-com');
+const gameScreen = document.querySelector('.game__screen');
 
 avatarSelectionBox.forEach((box) => {
   box.addEventListener('click', () => {
@@ -88,12 +91,40 @@ avatarSelectionBox.forEach((box) => {
 startGameButton.addEventListener('click', () => {
   avatarScreen.classList.add('fadeOut');
   vsScreen.classList.remove('hidden');
-
   avatarScreen.addEventListener('animationend', () => {
     avatarScreen.classList.add('hidden');
     vsScreen.classList.add('fadeIn');
     vsLeftSide.classList.add('slide-right');
     vsRightSide.classList.add('slide-left');
     vsMiddleText.classList.add('slide-middle');
+  });
+});
+
+/////////////////////////////////////////////////////////////////////////////////////
+// // // // // // // // // // // VERSUS SECTION // // // // // // // // // // // //
+////////////////////////////////////////////////////////////////////////////////////
+
+vsMiddleText.addEventListener('animationend', () => {
+  vsPlayerAvatar.classList.add('show-avatar');
+  vsComAvatar.classList.add('show-avatar');
+});
+
+vsComAvatar.addEventListener('animationend', () => {
+  vsLeftSide.classList.replace('slide-right', 'slide-right-reversed');
+  vsRightSide.classList.replace('slide-left', 'slide-left-reversed');
+  vsMiddleText.classList.replace('slide-middle', 'slide-middle-reversed');
+  gameScreen.classList.remove('hidden');
+  vsMiddleText.addEventListener('animationend', () => {
+    vsScreen.classList.add('fadeOut');
+    gameScreen.classList.add('fadeIn');
+    vsScreen.addEventListener('animationend', () => {
+      vsScreen.classList.remove('fadeIn');
+      vsScreen.classList.add('hidden');
+      vsLeftSide.classList.remove('slide-right-reversed');
+      vsRightSide.classList.remove('slide-left-reversed');
+      vsMiddleText.classList.remove('slide-middle-reversed');
+      vsPlayerAvatar.classList.remove('show-avatar');
+      vsComAvatar.classList.remove('show-avatar');
+    });
   });
 });
