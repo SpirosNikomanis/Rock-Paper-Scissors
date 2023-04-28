8c8664037e7c05407eed7747605efa5034632ffe
NSpiros <126255001+NSpiros@users.noreply.github.com>
2023-04-28 10:49:16 +0300

    bundler imported

---
+++ src/index.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: 'AIzaSyBL1WdGE-0FUkr7cAMx08DqDTUlSdS3zZs',
+  authDomain: 'rps-highscores-e7e4f.firebaseapp.com',
+  projectId: 'rps-highscores-e7e4f',
+  storageBucket: 'rps-highscores-e7e4f.appspot.com',
+  messagingSenderId: '953807012586',
+  appId: '1:953807012586:web:c157cb9ac8b8ea357e4d93',
+};
